5149c44895068ab93f84a7c49e60bfd456c9be6f
Wei <weizheng777@gmail.com>
2025-02-05 21:30:41 -0800

    add disappear animation

--- apple.gd
+++ apple.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 func _on_body_entered(body):
-	$AnimatedSprite2D.play( "colect")
-	await $AnimatedSprite2D.animation_finished# Wait for the animation to finish
+	animated_sprite_2d.animation =  "colect"
+	await animated_sprite_2d.animation_finished# Wait for the animation to finish
 	queue_free()  # Remove the item after the animation
 
 
