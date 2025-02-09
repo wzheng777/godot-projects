6263e7e0ef77fbc404327a5fb32e8de05d1e5595
Wei Zheng <weizheng777@gmail.com>
2025-02-09 14:56:43 -0800

    add new file

--- end.gd
+++ end.gd
@@ -1,10 +1,7 @@
 extends Area2D
 
-@onready var sprite_2d = $Sprite2D
+@export var target_level : PackedScene
 
 func _on_body_entered(body):
 	if (body.name == "MC"):
-		sprite_2d.animation =  "colect"
-		await sprite_2d.animation_finished# Wait for the animation to finish
-		queue_free()  # Remove the item after the animation
-
+		get_tree().change_scene_to_packed(target_level)
---
+++ main_menu.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+
+
+func _on_lvl_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://level1.tscn")
